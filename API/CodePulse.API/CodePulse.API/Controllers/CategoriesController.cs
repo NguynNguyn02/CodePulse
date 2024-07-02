c0aa89394c6c65b7fea9678796d8e443b7f1a73f
NguynNguyn02 <trungnguyenna02@gmail.com>
2024-07-03 01:46:58 +0700

    Initial Project

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.ImplementationReponsitory;
+using CodePulse.API.Repositories.InterfaceRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDTO request)
+        {
+            //MAP DTO to domain model
+
+            var category = new Category()
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+            };
+            await _categoryRepository.CreateAsync(category);
+
+            // Domain model to DTO
+
+            var response = new CategoryDTO()
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle,
+            };
+            return Ok(response);
+        } 
+    }
+}
---
+++ API/CodePulse.API/CodePulse.API/Repositories/ImplementationReponsitory/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.InterfaceRepository;
+
+namespace CodePulse.API.Repositories.ImplementationReponsitory
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await _db.Categories.AddAsync(category);
+            await _db.SaveChangesAsync();
+            return category;
+        }
+    }
+}
