67bf0a0594dbfb2553d932188c75a097428a5547
NguynNguyn02 <trungnguyenna02@gmail.com>
2024-07-04 07:22:19 +0700

    action get all category

--- UI/codepulseUI/src/app/features/category/add-category/add-category.component.ts
+++ UI/codepulseUI/src/app/features/category/add-category/add-category.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
 import { Subscription } from 'rxjs';
-import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -14,7 +13,7 @@ export class AddCategoryComponent implements OnDestroy {
   model: AddCategoryRequest;
   private addCategorySubscription?:Subscription;
 
-  constructor(private categoryService: CategoryService,private router:Router) {
+  constructor(private categoryService: CategoryService) {
     this.model = {
       name: "",
       urlHandle: ""
@@ -26,7 +25,7 @@ export class AddCategoryComponent implements OnDestroy {
     this.categoryService.addCategory(this.model)
     .subscribe({
       next:(response)=>{
-        this.router.navigateByUrl('/admin/categories');
+        console.log('This was successful!');
       }
     })
   }
--- UI/codepulseUI/src/app/features/category/category-list/category-list.component.ts
+++ UI/codepulseUI/src/app/features/category/category-list/category-list.component.ts
@@ -14,7 +14,7 @@ export class CategoryListComponent implements OnInit {
   constructor(private categoryService: CategoryService) {
   }
   ngOnInit(): void {
-    this.categories$ = this.categoryService.getAllCategory();
+    this.categoryService.getAllCategory();
   };
 }
 
