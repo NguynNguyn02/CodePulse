1c8b9a7b92ed0e97e5f967635f22467d768f6f7b
NguynNguyn02 <trungnguyenna02@gmail.com>
2024-07-09 03:31:18 +0700

    Edit Update category UI

--- UI/codepulseUI/src/app/app.module.ts
+++ UI/codepulseUI/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { CategoryListComponent } from './features/category/category-list/categor
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { EditcategoryComponent } from './features/category/editcategory/editcategory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CategoryListComponent,
-    AddCategoryComponent
+    AddCategoryComponent,
+    EditcategoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ UI/codepulseUI/src/app/features/category/editcategory/editcategory.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
+
+@Component({
+  selector: 'app-editcategory',
+  templateUrl: './editcategory.component.html',
+  styleUrls: ['./editcategory.component.css']
+})
+export class EditcategoryComponent implements OnInit, OnDestroy {
+  id: string | null = null;
+  paramsSubcription?: Subscription;
+  editCategorySubcription?:Subscription;
+  category?: Category;
+  constructor(private route: ActivatedRoute, private categoryService: CategoryService,private router:Router) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.paramsSubcription = this.route.paramMap.subscribe({
+      next: (params) => {
+        this.id = params.get('id');
+        if (this.id) {
+          //get the data from the API for this category ID
+          this.categoryService.getCategoryById(this.id)
+            .subscribe({
+              next: (response) => {
+                this.category = response;
+              }
+            });
+        }
+      }
+    })
+  }
+
+
+  onFormSubmit() :void{
+    const updateCategoryRequest : UpdateCategoryRequest = {
+      name:this.category?.name ?? '',
+      urlHandle: this.category?.urlHandle?? ''
+    }
+    //pass this object to service
+    if(this.id){
+      this.editCategorySubcription = this.categoryService.UpdateCategory(this.id,updateCategoryRequest)
+      .subscribe({
+        next:reponse=>{
+          this.router.navigateByUrl('/admin/categories')
+        }
+      })
+
+    }
+  } 
+  ngOnDestroy(): void {
+    this.paramsSubcription?.unsubscribe();
+    this.editCategorySubcription?.unsubscribe();
+  }
+}
