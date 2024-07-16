92258a59b5f36777b960d5c8bd21d5dffdc156b8
NguynNguyn02 <trungnguyenna02@gmail.com>
2024-07-16 08:50:48 +0700

    finish-p10

--- UI/codepulseUI/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ UI/codepulseUI/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { Observable } from 'rxjs';
 import { BlogPostService } from '../services/blog-post.service';
 import { Router } from '@angular/router';
+import { Category } from '../../category/models/category.model';
+import { CategoryService } from '../../category/services/category.service';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnInit{
   model: AddBlogPost;
-  constructor(private blogPostService: BlogPostService,private router:Router) {
+  categories$?:Observable<Category[]>;
+  constructor(private blogPostService: BlogPostService,private router:Router,private categoryService:CategoryService) {
     this.model = {
       title: '',
       shortDescription: '',
@@ -20,8 +23,12 @@ export class AddBlogpostComponent {
       featuredImageUrl: '',
       author: '',
       isVisible: true,
-      publishedDate: new Date()
+      publishedDate: new Date(),
+      categories:[]
     }
+  }
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategory();
   }
   onFormSubmit(): void {
     this.blogPostService.createBlogPost(this.model)
