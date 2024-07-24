a12929bc9facca5896c44926908ecc569f1ea7b8
NguynNguyn02 <trungnguyenna02@gmail.com>
2024-07-24 18:19:48 +0700

    display images into selector images  UI

--- UI/codepulseUI/src/app/shared/components/image-selector/image-selector.component.ts
+++ UI/codepulseUI/src/app/shared/components/image-selector/image-selector.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ImageService } from './image.service';
+import { Observable } from 'rxjs';
+import { BlogImage } from '../../models/blog-image.models';
+import { NgFor } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-image-selector',
   templateUrl: './image-selector.component.html',
   styleUrls: ['./image-selector.component.css']
 })
-export class ImageSelectorComponent {
+export class ImageSelectorComponent implements OnInit {
   
   private file?: File;
   fileName : string = '';
   title : string = '';
+  images$?:Observable<BlogImage[]>
+
+  @ViewChild('form',{static:false}) imageUploadForm?:NgForm;
   
   constructor(private imageService:ImageService){
 
+  }
+  ngOnInit(): void {
+    this.getAllImages();
   }
   
   onFileUploadChange(event : Event):void{
@@ -21,14 +31,21 @@ export class ImageSelectorComponent {
     this.file = element.files?.[0];
   }
 
+  selectImage(){
+    
+  }
   uploadImage():void{
     if(this.file && this.fileName !== '' && this.title !== ''){
       // Image service to upload the image
       this.imageService.uploadImage(this.file,this.fileName,this.title).subscribe({
         next: (response)=>{
-          console.log(response);
+          this.imageUploadForm?.resetForm();
+          this.getAllImages();
         }
       });
     }
+  }
+  private getAllImages(){
+    this.images$ = this.imageService.getAllImages();
   }
 }
