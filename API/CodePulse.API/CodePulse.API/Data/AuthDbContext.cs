16ae837f7f9be34b0d0cc0f24da8af87e73452e0
NguynNguyn02 <trungnguyenna02@gmail.com>
2024-08-14 02:44:36 +0700

    Setting up Auth + Create Dbcontext,Seed Roles

---
+++ API/CodePulse.API/CodePulse.API/Data/AuthDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "9f99dfbc-8c7b-4822-a4ac-54eacbdf00e6";
+            var writerRoleId = "f8056a5a-a4b4-4262-9cd9-d8134520985d";
+
+            //create Reader and Writer Role
+            var roles = new List<IdentityRole>
+                {
+                new IdentityRole()
+                {
+                    Id = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper(),
+                    ConcurrencyStamp = readerRoleId
+                },
+                new IdentityRole()
+                {
+                    Id=writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Write".ToUpper(),
+                    ConcurrencyStamp = writerRoleId
+                }
+            };
+
+            //Seed the roles
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
--- API/CodePulse.API/CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
+++ API/CodePulse.API/CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b6d0d1e7390bcaf2233b05c083fa9eea3b6e42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337d13e50efb54c0c663b4cae3ad2e5878745c86")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
